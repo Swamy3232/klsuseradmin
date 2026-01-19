cc42382b34955b5af1d2df31071edafdcd66e84c
Swamy R <swamiaws85@gmail.com>
2026-01-19 09:12:25 +0530

    done

--- src/pages/payment.jsx
+++ src/pages/payment.jsx
@@ -9,7 +9,7 @@ export default function UPITest() {
       return;
     }
 
-    const upiUrl = `upi://pay?pa=9019740523@ybl&pn=Swamy&am=${amount}&cu=INR&tn=Test Payment`;
+    const upiUrl = `upi://pay?pa=7483673954@ybl&pn=Swamy&am=${amount}&cu=INR&tn=Test Payment`;
 
     window.location.href = upiUrl;
   };
