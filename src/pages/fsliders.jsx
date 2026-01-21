09a0501b341efbc4b9b440f938b55d34cc7efdb5
Swamy R <swamiaws85@gmail.com>
2026-01-21 14:52:19 +0530

    dsf

--- src/pages/fsliders.jsx
+++ src/pages/fsliders.jsx
@@ -179,7 +179,7 @@ const KlsGoldSlider = () => {
                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
                       </svg>
                     </div>
-                    <span className="text-yellow-200 text-xs font-medium">LIVE</span>
+                    <span className="text-yellow-200 text-xs font-medium">Click to Live Prices</span>
                   </div>
                 </div>
               </button>
