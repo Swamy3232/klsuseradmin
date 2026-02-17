89560f1b8a6954784d05c1af59a00bc37be6f604
Swamy R <swamiaws85@gmail.com>
2026-02-17 14:22:01 +0530

    UPDATED

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -709,7 +709,7 @@ const KLSGoldCollections = () => {
                 </button>
               </div>
             ) : viewMode === 'grid' ? (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 {filteredCollections.map(item => (
                   <div 
                     key={item.id} 
